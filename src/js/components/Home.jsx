fb3334eda497d78df8503f00f898cf2726b26802
AldairMoralesAlfaro <ald_brake@hotmail.com>
2025-07-09 04:51:26 +0000

    cree tareas

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -27,8 +27,36 @@ const Home = () => {
 				if (createTask ===""){
 					return;
 				}
-			setTasks([...tasks, createTask.trim()])
-			setCreateTask("")
+			// setTasks([...tasks, createTask.trim()])
+			// setCreateTask("")
+      fetch('https://playground.4geeks.com/todo/todos/alda', {
+      method: "POST",
+      body: JSON.stringify({
+        label: createTask,
+        is_done: false
+      }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    .then(resp => {
+        console.log(resp.ok);
+        if (resp.ok){
+          //onload()
+          setCreateTask("")
+        } // Será true si la respuesta es exitosa
+        console.log(resp.status); // El código de estado 201, 300, 400, etc.
+        return resp.json(); // Intentará parsear el resultado a JSON y retornará una promesa donde puedes usar .then para seguir con la lógica
+    })
+    .then(data => {
+        // Aquí es donde debe comenzar tu código después de que finalice la búsqueda
+        console.log(data); // Esto imprimirá en la consola el objeto exacto recibido del servidor
+      	setTasks([...tasks, data])
+    })
+    .catch(error => {
+        // Manejo de errores
+        console.log(error);
+    });
 		}
 	}
 	const deleteTask = (index) => {
